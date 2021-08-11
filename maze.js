457e01d8a955e17fac5e6c3c062f6281ced07dc4
Quang Tran <qut222@lehigh.edu>
2021-08-11 16:52:18 -0400

    Finish DFS maze generation

--- maze.js
+++ maze.js
@@ -1,6 +1,6 @@
 let maze = document.querySelector(".maze");
 let context = maze.getContext("2d");
-
+let curr;
 
 /**
  * Cell class, class for the cells in the maze
@@ -21,62 +21,151 @@ class Cell{
         };
 
     }
+
+    checkNB(){
+        let grid = this.pGrid;
+        let row = this.rowI;
+        let col = this.colI;
+        let nb = []; 
+
+        //assign neigbnour and handle if cell is on edge of grid
+        let top = row !== 0 ? grid[row-1][col] : undefined;
+        let right = col !== grid.length - 1  ? grid[row][col+1] : undefined;
+        let bot = row !== grid.length -1 ? grid[row+1][col] : undefined;
+        let left = col !== 0 ? grid[row][col-1] : undefined;
+
+        if(top && !top.status ){
+            nb.push(top);
+        }
+
+        if(right && !right.status ){
+            nb.push(right);
+        }
+
+        if(bot && !bot.status ){
+            nb.push(bot);
+        }
+
+        if(left && !left.status ){
+            nb.push(left);
+        }
+
+        //select and return a random neighbour 
+        if(nb.length !==0){
+            let random = Math.floor(Math.random()*nb.length);
+            return nb[random];
+        } else{
+            return undefined;
+        }
+    
+
+
+
+    }
+    deleteBorder(c1,c2){
+        let x = c1.colI - c2.colI;
+        let y = c1.rowI - c2.rowI;
+
+        if(x==1){
+            c1.borders.lb = false;
+            c2.borders.rb = false;
+        }
+        else if(x==-1){
+            c1.borders.rb = false;
+            c2.borders.lb = false;
+        }
+
+        if(y==1){
+            c1.borders.tb = false;
+            c2.borders.bb = false;
+        }
+        else if(y==-1){
+            c1.borders.bb = false;
+            c2.borders.tb = false;
+        }
+        
+    }
+
+    highlight(columns){
+        let x = (this.colI * this.gridSize) / columns + 1;
+        let y = (this.rowI * this.gridSize) / columns + 1;
+
+        context.fillStyle = "yellow";
+        context.fillRect(x,y,this.gridSize/columns - 3, this.gridSize/ columns -3);
+    }
+
+
     /**
-     * method to generate borders around cells
+     * methods to generate borders around cells
      * x is col coordinate, y is row coordinate
      * dir is which direction do you want to build the border in
      * colSize and rowSize refers to number of columns and rows on grid
      * size refers to area of the grid 
      */
     
-    createBorder(x,y,size, colSize, rowSize, dir){
-        if(dir == "top"){
-            context.beginPath();
-            context.moveTo(x,y);
-            context.lineTo(x+size/colSize,y);
-            context.stroke();
-        } else if(dir == "bottom"){
-            context.beginPath();
-            context.moveTo(x,y-size/rowSize);
-            context.lineTo(x + size/colSize);
-            context.stroke();
-        } else if(dir == "right"){
-            context.beginPath();
-            context.moveTo(x+size/colSize,y);
-            context.lineTo(x+size/colSize,y-size/rowSize);
-            context.stroke();
-        } else if(dir == "left"){
-            context.beginPath();
-            context.moveTo(x,y);
-            context.lineTo(x,y-size/rowSize);
-            context.stroke();
-        }
+    createBorderTop(x,y,size, colSize, rowSize){
+     
+        context.beginPath();
+        context.moveTo(x,y);
+        context.lineTo(x+size/colSize,y);
+        context.stroke();
+        
 
+    }
+    createBorderBot(x,y,size, colSize, rowSize){
 
+        context.beginPath();
+        context.moveTo(x,y+size/rowSize);
+        context.lineTo(x + size/colSize,y+size/rowSize);
+        context.stroke();
+    
+
+    }
+    createBorderRight(x,y,size, colSize, rowSize){
+     
+        context.beginPath();
+        context.moveTo(x+size/colSize,y);
+        context.lineTo(x+size/colSize,y+size/rowSize);
+        context.stroke();
 
 
     }
+    createBorderLeft(x,y,size, colSize, rowSize){
+     
+  
+        context.beginPath();
+        context.moveTo(x,y);
+        context.lineTo(x,y+size/rowSize);
+        context.stroke();
+
+    }
+
 
     drawBorder(size, colSize,rowSize){
         //get coordinate of top right corner of cell in grid 
-        let x = (this.colNum * size) / colSize;
-        let y = (this.rowNum * size) / rowSize;
+        let x = (this.colI * size) / colSize;
+        let y = (this.rowI * size) / rowSize;
         
         context.strokeStyle = "white";
         context.fillStyle = "black";
         context.lineWidth = 2;
-
+        
         if(this.borders.tb){
-            this.createBorder(x,y,size,colSize,rowSize,"top");
+            this.createBorderTop(x,y,size,colSize,rowSize);
+            
         }
         if(this.borders.bb){
-            this.createBorder(x,y,size,colSize,rowSize,"bottom");
+            this.createBorderBot(x,y,size,colSize,rowSize);
         }
         if(this.borders.lb){
-            this.createBorder(x,y,size,colSize,rowSize,"left");
+            this.createBorderLeft(x,y,size,colSize,rowSize);
         }
         if(this.borders.rb){
-            this.createBorder(x,y,size,colSize,rowSize,"right");
+            this.createBorderRight(x,y,size,colSize,rowSize);
+        }
+        if(this.status){
+            //+1 and -2 is so that when it fills the box it doesnt fill the borders
+            context.fillRect(x+1,y+1, size/colSize -2, size/rowSize-2);
         }
 
 
@@ -89,24 +178,70 @@ class Cell{
  */
 class Maze{
     constructor(size, rows, cols){
-        this.size = size;
-        this.cols = cols;
-        this.rows = rows;
+        this.size = size; // number of pixels
+        this.cols = cols; // number of cols
+        this.rows = rows; // number of rows
         this.grid = [];
         this.stacks = []; //stack for backtracking
     }
 
     //method to fill out grid with cells
-    init(){
+    setup(){
         for(let y = 0; y < this.rows; y++ ){
             let row = []
             for(let x = 0; x < this.cols; x++){
-                let cell = new Cell(y,x,this.grid,this.gridSize);
+                let cell = new Cell(y,x,this.grid,this.size);
                 row.push(cell);
             }
             this.grid.push(row);
         }
         curr = this.grid[0][0];
     }
+    draw(){
+        maze.width = this.size;
+        maze.height = this.size;
+        maze.style.background = "black";
+        curr.status = true;
+
+        for (let r = 0; r < this.rows; r++){
+            for(let c = 0; c < this.cols; c++){
+                let grid = this.grid;
+                grid[r][c].drawBorder(this.size,this.cols,this.rows);
+            }
+        }
+
+        let next = curr.checkNB();
+        if(next){
+            next.visited = true;
+
+            this.stacks.push(curr);
+
+            curr.highlight(this.cols);
+            curr.deleteBorder(curr,next);
+            curr = next; 
+        //back tracking if theres no valid neighbours 
+        } else if (this.stacks.length > 0){
+            let cell = this.stacks.pop();
+            curr  = cell;
+            curr.highlight(this.cols);
+        } 
+
+        //finish drawing maze
+        if(this.stacks.length==0){
+            let x = (curr.colI * curr.gridSize) / this.cols + 1;
+            let y = (curr.rowI * curr.gridSize) / this.cols + 1;
+    
+            context.fillStyle = "black";
+            context.fillRect(x,y,curr.gridSize/this.cols - 3, curr.gridSize/this.cols -3);
+            return;
+        }
+
+        window.requestAnimationFrame(()=>{
+            this.draw();
+        })
+    }
 
 }
+let newMaze = new Maze(500,10,10);
+newMaze.setup();
+newMaze.draw();
